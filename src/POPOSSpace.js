eba457afa50fd26b2cd00c94d007aaf442187f29
nehakhan21 <60405329+nehakhan21@users.noreply.github.com>
2020-04-27 19:01:56 -0700

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,15 +2,16 @@
 
 import React from 'react'
 import './POPOSSpace.css'
-    function POPOSSpace(props) {
-      const { name, image, address } = props
-      return (
-        <div className="POPOSSpace">
-          <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-          <h1>{name}</h1>
-          <div>{address}</div>
-        </div>
-      )
-    }
-
+      function POPOSSpace(props) {
+    // Get the
+    const { name, image, address, hours } = props
+    return (
+      <div className="POPOSSpace">
+        <h1>{name}</h1>
+        <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+        <div>{address}</div>
+        <div>{hours}</div>
+      </div>
+    )
+  }
 export default POPOSSpace
